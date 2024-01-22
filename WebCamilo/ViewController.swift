da9514c0d03db0be381ce2833a405b7f2eaf51d0
camilo <camilo@Mac-de-camilo.local>
2024-01-22 11:49:19 -0800

    Initial Commit

---
+++ WebCamilo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WebCamilo
+//
+//  Created by camilo on 1/22/24.
+//  Copyright © 2024 camilo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
