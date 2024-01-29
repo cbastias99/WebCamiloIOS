083d66f0f3da9b8a425b2a0240425730a33aa403
= <cbastias99@gmail.com>
2024-01-29 12:27:26 -0300

    fidel nadal

--- WebCamilo/ViewController.swift
+++ WebCamilo/ViewController.swift
@@ -32,36 +32,7 @@ class ViewController: UIViewController, WKNavigationDelegate {
          print ("hola2")
 
         //
-         func getIfConfigOutput() -> [String:String] {
-    let cmd = "for i in $(ifconfig -lu); do if ifconfig $i | grep -q \"status: active\" ; then echo $i; fi; done"
-    let interfaceString = shell(cmd)
-    let interfaceArray = interfaceString.components(separatedBy: "\n")
-    var finalDictionary:[String:String] = [String:String]()
-    for (i,_) in interfaceArray.enumerated() {
-        if (interfaceArray[i].hasPrefix("en")){
-            let sp = shell("ifconfig \(interfaceArray[i]) | grep \"inet \" | grep -v 127.0.0.1 | cut -d\\  -f2")
-          finalDictionary[interfaceArray[i]] = sp.replacingOccurrences(of: "\n", with: "")
-        }
-    }
-  print(finalDictionary)
-    return finalDictionary
-}
-func shell(_ args: String) -> String {
-    var outstr = ""
-    let task = Process()
-    task.launchPath = "/bin/sh"
-    task.arguments = ["-c", args]
-    let pipe = Pipe()
-    task.standardOutput = pipe
-    task.launch()
-    let data = pipe.fileHandleForReading.readDataToEndOfFile()
-    if let output = NSString(data: data, encoding: String.Encoding.utf8.rawValue) {
-        outstr = output as String
-    }
-    task.waitUntilExit()
-    return outstr
 
-}
         let url = URL(string : "https://www.elc.cl/elcapp1/login.php")!
 
         webView.load(URLRequest(url:url))
