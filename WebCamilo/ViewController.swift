3f6fabeb243268b4393800a7c8ffe1fd16cb4744
= <cbastias99@gmail.com>
2024-01-29 10:47:37 -0300

    seggundoo

--- WebCamilo/ViewController.swift
+++ WebCamilo/ViewController.swift
@@ -9,7 +9,7 @@
 import UIKit
 import WebKit
 import Foundation
-import Network
+import ifaddrs      
 
 class ViewController: UIViewController, WKNavigationDelegate {
     var webView: WKWebView!
@@ -20,14 +20,52 @@ class ViewController: UIViewController, WKNavigationDelegate {
     
     override func viewDidLoad() {
        print("hola")
-        
+func getLocalIPAddress() -> String? {
+    var address: String?
+
+    // Get list of all interfaces on the local machine
+    var ifaddr: UnsafeMutablePointer<ifaddrs>?
+    guard getifaddrs(&ifaddr) == 0 else { return nil }
+    guard let firstAddr = ifaddr else { return nil }
+
+    // Iterate through the list of interfaces
+    for ptr in sequence(first: firstAddr, next: { $0.pointee.ifa_next }) {
+        let flags = Int32(ptr.pointee.ifa_flags)
+        let addr = ptr.pointee.ifa_addr.pointee
+
+        // Check for running IPv4 or IPv6 interfaces
+        if (flags & (IFF_UP | IFF_RUNNING | IFF_LOOPBACK)) == (IFF_UP | IFF_RUNNING) {
+            if addr.sa_family == UInt8(AF_INET) || addr.sa_family == UInt8(AF_INET6) {
+                var hostname = [CChar](repeating: 0, count: Int(NI_MAXHOST))
+                if (getnameinfo(ptr.pointee.ifa_addr, socklen_t(addr.sa_len),
+                                &hostname, socklen_t(hostname.count),
+                                nil, socklen_t(0), NI_NUMERICHOST) == 0) {
+                    address = String(cString: hostname)
+                    break
+                }
+            }
+        }
+    }
+
+    freeifaddrs(ifaddr)
+
+    return address
+}
+
+// Example usage
+if let localIPAddress = getLocalIPAddress() {
+    print("Local IP Address: \(localIPAddress)")
+} else {
+    print("Unable to retrieve local IP address.")
+}
+
         guard let urlpost = URL(string: "https://www.elc.cl/test2/iOS/registrarTelefono.php") else {
             return
         }
         
-        
-        let user1 = "12345"
-        let pass = "192.168."
+          let user1 = "12345"
+        let pass = localIPAddress
+       
         print(user1)
         print(pass)
         let data : Data = "telefono=\(user1)&ip=\(pass)&grant_type=password".data(using: .utf8)!
@@ -38,33 +76,9 @@ class ViewController: UIViewController, WKNavigationDelegate {
         request.httpBody = data
         
         print("one called")
-        func getLocalIPAddress() -> String? {
-            var ipAddress: String?
-
-            let nwPath = NWPathMonitor()
-            let queue = DispatchQueue(label: "Monitor")
-            nwPath.start(queue: queue)
-
-            nwPath.pathUpdateHandler = { path in
-                if let ipv4Interface = path.availableInterfaces.filter({ $0.type == .wifi || $0.type == .wiredEthernet }).first,
-                let ipv4Address = ipv4Interface.ipv4Addresses.first {
-                    ipAddress = ipv4Address
-                }
-            }
-
-            return ipAddress
-        }
-
-        // Example usage
-        if let localIPAddress = getLocalIPAddress() {
-            print("Local IP Address: \(localIPAddress)")
-        } else {
-            print("Unable to retrieve local IP address.")
-        }
-                let config = URLSessionConfiguration.default
-                let session = URLSession(configuration: config)
-                // vs let session = URLSession.shared
-                // make the request
+       
+ 
+      
                 
         let task = session.dataTask(with: request, completionHandler: {
             (data, response, error) in
